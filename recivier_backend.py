895f7d9a628990f246189480bdc6cc05f542e7f4
Ming <ming2007119@gmail.com>
2023-12-11 15:27:36 +0800

    backend

--- recivier_backend.py
+++ recivier_backend.py
@@ -1,11 +1,100 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from grove.adc import ADC
+from time import sleep
+import asyncio
+import timeit
+from hamming_code import hamming_decode
+from bitarray import bitarray
 
+# === LIGHT SENSOR SETTING ===
+threshold = 300
+channel = 2
+adc = ADC()
+timer = timeit.default_timer
+decoded_message = ''
+
+async def read_header(channel, adc) -> float:
+  count    = 0
+  interval = 0
+
+  while adc.read(channel) < threshold:
+    pass
+
+  while True:
+    start = timer()
+    while adc.read(channel) > threshold:
+      pass
+
+    interval += timer() - start
+    count += 1
+
+    if count == 16: break
+    while adc.read(channel) < threshold:
+      pass
+  
+  return interval / count
+
+async def read_length(channel, adc, interval) -> int:
+  count      = 0
+  length_str = ""
+
+  while adc.read(channel) < threshold:
+    pass
+
+  while True:
+    start = timer()
+    while adc.read(channel) > threshold:
+      pass
+
+    t = timer() - start
+    length_str += t > interval and "1" or "0"
+    count += 1
+
+    if count == 16: break
+    while adc.read(channel) < threshold:
+      pass
+  
+  return int(length_str, 2)
+
+async def read_data(channel, adc, interval, length) -> str:
+  count    = 0
+  data_str = ''
+
+  while adc.read(channel) < threshold:
+    pass
+
+  while True:
+    start = timer()
+    while adc.read(channel) > threshold:
+      pass
+
+    t = timer() - start
+    data_str += t > interval and "1" or "0"
+    count += 1
+
+    if count == length * 12: break
+    while adc.read(channel) < threshold:
+      pass
+
+  return data_str
+
+async def read_message():
+  global decoded_message
+  while True:
+    interval = await read_header(channel, adc)
+    length = await read_length(channel, adc, interval)
+    start = timer()
+    message = await read_data(channel, adc, interval, length)
+    end = timer()
+    decoded_message = hamming_decode(message)
+    print(decoded_message, end = '')
+    print(f"bps: {length*12 / (end - start)}")
+# ============================
+
+# === WEB SERVER SETTING ===
 app = FastAPI()
-origins = [
-  "http://localhost:3000",
-]
+origins = ["http://localhost:3000",]
 app.add_middleware(
   CORSMiddleware,
   allow_origins=origins,
@@ -14,13 +103,17 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-  return {"message": "Hello World"}
-
 @app.post("/get_recive")
-def send():
-  return {"message": "TBD"}
+def get_recive():
+  global decoded_message
+  message = decoded_message
+  decoded_message = ''
+  return {"message": message}
+
+@app.on_event("startup")
+async def startup_event():
+  asyncio.create_task(read_message())
+# ==========================
 
 if __name__ == "__main__":
   import uvicorn
--- sender_backend.py
+++ sender_backend.py
@@ -1,12 +1,36 @@
+from time import sleep
+from gpiozero import LED
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from hamming_code import hamming_encode, hamming_decode
+from hamming_code import hamming_encode
+from fastapi.middleware.cors import CORSMiddleware
 
+# === LED SETTING ===
+# FIXME: gpio busy when init led
+# led = LED(13)
+
+def add_header(msg: str):
+  preamble = f'{0xAAAA:0>16b}'
+  length   = f'{len(msg):0>16b}'
+  return preamble + length + msg
+
+# FIXME: ref to above FIXME
+# def send_msg(msg: str):
+#   for i in msg:
+#     if i == '1':
+#       led.on()
+#       sleep(0.005)
+#       led.off()
+#     else:
+#       led.on()
+#       sleep(0.003)
+#       led.off()
+#     sleep(0.003)
+# ===================
+
+# === WEB SERVER SETTING ===
 app = FastAPI()
-origins = [
-  "http://localhost:3000",  # frontend
-]
+origins = ["http://localhost:3000",]
 app.add_middleware(
   CORSMiddleware,
   allow_origins=origins,
@@ -18,16 +42,18 @@ app.add_middleware(
 class Data(BaseModel):
   message: str
 
-@app.get("/")
-def read_root():
-  return {"message": "Hello World"}
-
 @app.post("/send")
-def send(data: Data):
+async def send(data: Data):
   encoded_message = hamming_encode(data.message)
+  package = add_header(encoded_message)
+  
+  # TODO: add header to encoded_message
+  # then send the message to recivier
+  # FIXME: ref to above FIXME
+  # await send_msg(package)
 
-
-  return {"message": "Message received"}
+  return {"message": "Message sent"}
+# ==========================
 
 if __name__ == "__main__":
   import uvicorn
